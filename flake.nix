04d7313b145bead58c0e92bf6bb143f3b92cc76b
Gregers Rørdam <gregers@rordam.dk>
2026-05-08 08:12:41 +0200

    No longer apply fastmcp 3.0 patch

--- flake.nix
+++ flake.nix
@@ -10,13 +10,7 @@
       system = "x86_64-linux";
       myLib = import ./lib.nix { flakePkgs = nixpkgs; inherit system; };
       unpatchedPkgs = nixpkgs.legacyPackages.${system};
-      pkgs = myLib.patch [
-      # Patch to "merge in" PR 510339 to update fastmcp to >3.0
-          (unpatchedPkgs.fetchpatch2 {
-            url = "https://github.com/NixOS/nixpkgs/compare/28a868327158313f3a38f775e13866979da79cca...977ebf6a890866ac9cc67d48b4f876c1c3dee652.diff?full_index=1";
-            hash = "sha256-g50+6Ib4vft7oCMkx1d+hxGTqnS0vop/M7yrEv6ddzM=";
-          })
-      ];
+      pkgs = unpatchedPkgs;
       packages = import ./default.nix { inherit pkgs;};
     in {
       packages.x86_64-linux = packages;
